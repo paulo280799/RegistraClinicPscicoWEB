f89311781589e4cf6719d3da50c83d8ae3c7acbb
paulo280799 <34691986+paulo280799@users.noreply.github.com>
2019-09-16 13:44:34 -0300

    Modal feito

--- RegistraClinicPsico_WEB/telas/TelaPaciente.php
+++ RegistraClinicPsico_WEB/telas/TelaPaciente.php
@@ -146,7 +146,32 @@
         <?php if(isset($_GET['editar'])):?>
           <button class="btn btn-primary" type="submit" name="atualizar" value="atualizar">Atualizar</button>
         <?php else : ?>
-          <button class="btn btn-primary" type="submit" name="cadastrar" value="cadastrar">Cadastrar</button>
+          <!-- Button trigger modal -->
+          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+            Cadastrar
+          </button>
+
+          <!-- Modal -->
+          <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  ...
+                </div>
+                <div class="modal-footer">
+                  <button class="btn btn-primary" type="submit" name="cadastrar" value="cadastrar">Gerar Comprovante</button>
+                  <button class="btn btn-primary" type="submit" name="cadastrar" value="cadastrar">Cadastrar</button>
+                </div>
+              </div>
+            </div>
+          </div>
+            
         <?php endif; ?>
 
         <input class="btn btn-primary" type="reset" value="Limpar">
